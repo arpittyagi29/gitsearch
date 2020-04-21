42662d87fb8e42cffab6751eeb75a547e9da244c
Arpit Tyagi <51491402+arpittyagi29@users.noreply.github.com>
2020-04-21 14:40:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'; 
 import Front from './components/Front';
+import Testing from './components/Testing'
 
 
 function App() {
   return (
     <div className="App">
-      <Front/>
+<Testing/>
     </div>
   );
 }
